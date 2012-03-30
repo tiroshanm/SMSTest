0041baf6145b5d27abc353f411f5a7ed4c6a4173
Tiroshan <tiroshanm@gmail.com>
2012-03-30 21:40:41 +0530

    a

---
+++ StudentManagementSystem/src/studentmanagementsystem/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentmanagementsystem;
+
+/**
+ *
+ * @author Tiroshan
+ */
+
+
+import javax.naming.Context;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.naming.Context;
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+         ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("./Spring.xml");
+        context.start();
+        
+        StudentManagementSystem test=(StudentManagementSystem)context.getBean("StudentManagementSystem1");
+        
+    }
+}
